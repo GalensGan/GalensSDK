a532fb324438b86a57958cbee746e07c2e911095
galensgan <gmx_galens@163.com>
2020-11-25 23:52:58 +0800

    update

---
+++ GalensSDK/TimeEx/TimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalensSDK.TimeEx
+{
+    public static class TimeConverter
+    {
+        /// <summary>
+        /// 将时间转成时间戳
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static long ConvertToTimestamp(this DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+        }        
+    }
+
+    
+}
---
+++ GalensSDK/TimeEx/TimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalensSDK.TimeEx
+{
+    public class TimeHelper
+    {
+        /// <summary>
+        /// 获取当前时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long TimestampNow()
+        {
+            return DateTime.Now.ConvertToTimestamp();
+        }
+    }
+}
